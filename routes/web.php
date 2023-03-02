72589a6cc228a72ed72f7915e7e7fa0d51db8fed
Ahmed-Munna <ahmedmunna4200@gmail.com>
2023-03-02 11:09:52 +0600

    laravel for practis

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use APP\Http\Middleware\nameValide;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+Route::view('/about', 'about')->name('about.us');
+// Route::get('/contact/{roll}', function ($roll) {
+//     return "my roll is $roll";
+//     // return redirect('about');
+// });
+  
+Route::get('/contact', function () {
+    return view('contact');
+})->name('contact.list');
+
+// Route::get('/country', function () {
+//     return 'this is country page';
+// })->middleware('name');
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
